9e7bb8ec2cf293e31a764345a653688fd7f01eb8
Michael B <mbuelow42@protonmail.com>
2021-01-28 17:30:36 +0100

    NO-TICKET: removed usage of old "binary" directory

--- compile.sh
+++ compile.sh
@@ -2,11 +2,6 @@
 
 # Target RaspberryPi 2 or lower
 cargo build --release --target arm-unknown-linux-gnueabihf
-mkdir -p binaries/arm
-cp target/arm-unknown-linux-gnueabihf/release/dnsmdcd binaries/arm/dnsmdcd
-
 
 # Target RaspberryPi 3 or higher
 cargo build --release --target armv7-unknown-linux-gnueabihf
-mkdir -p binaries/armv7
-cp target/armv7-unknown-linux-gnueabihf/release/dnsmdcd binaries/armv7/dnsmdcd
